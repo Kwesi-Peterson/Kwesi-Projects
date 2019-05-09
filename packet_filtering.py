a6f11a3badba0f721c6bce6012db1b1d256f68b2
Kwesi Peterson <kwesipeterson@yahoo.co.uk>
2019-05-09 20:51:47 +0100

    Added graphs for plotting missing pings

--- packet_filtering.py
+++ packet_filtering.py
@@ -8,6 +8,8 @@ TODO rename module
 from collections import defaultdict
 import os
 import pyshark
+import re
+import matplotlib.pyplot as plt
 
 
 class FileNotFoundError(OSError):
@@ -307,3 +309,142 @@ def get_icmp_stats_ip_from_pcap(pcap_file, ip_addr):
     icmp_sent_to_stats, icmp_rcvd_from_stats = get_icmp_stats_ip_from_list(list_icmp_pkts, ip_addr)
 
     return icmp_sent_to_stats, icmp_rcvd_from_stats
+
+
+def get_list_icmp_requests_no_response(pcap_file):
+    """
+    A function to get a list of ICMP requests that have no response.
+    :param pcap_file:   A .pcap file e.g. C:/my_pcap_files/'my_capture.pcap'.
+    :return:            A list of ICMP requests that have no response.
+    """
+    file_cap = get_file_cap_from_pcap_file(pcap_file)
+    list_icmp_pkts = filter_pkts_icmp(file_cap)
+    list_icmp_req_pkts = filter_icmp_pkts_requests(list_icmp_pkts)
+    list_icmp_reply_pkts = filter_icmp_pkts_replies(list_icmp_pkts)
+    list_icmp_req_no_reply = []
+
+    dict_reply_ident_seq = defaultdict(list)
+    for reply_pkt in list_icmp_reply_pkts:
+        if reply_pkt.icmp.ident != 0:
+            dict_reply_ident_seq[reply_pkt.icmp.ident].append(reply_pkt.icmp.seq)
+
+    for req_pkt in list_icmp_req_pkts:
+        ident = req_pkt.icmp.ident
+        seq = req_pkt.icmp.seq
+        if not seq in dict_reply_ident_seq[ident]:
+            list_icmp_req_no_reply.append(req_pkt)
+
+    return list_icmp_req_no_reply
+
+
+def convert_time_ns_ms(timestamp_ns):
+    """
+    A function to convert a timestamp with nanoseconds into a timestamp with
+    milliseconds.
+    :param timestamp_ns: A timestamp in the format HH:MM:SS.XXXXXXXXX.
+    :return:             A simplified timestamp, with milliseconds rather
+                         than nanoseconds.
+    """
+    ms = timestamp_ns[:-6]  # Remove 6 digits to convert nano to milliseconds.
+    return ms
+
+
+def simplify_long_timestamp(long_timestamp):
+    """
+    A function to simplify a timestamp into HH:MM:SS.XXXXXX format.
+    We convert it into microsecond format because the datetime.strptime
+    function can't deal with smaller increments of time than microseconds.
+    :param long_timestamp: A string with a full timestamp, including the
+                           time in a HH:MM:SS.XXXXXXXXX.
+    :return:               A simplified timestamp.
+    """
+    match = re.search('(2[0-3]|[01]?[0-9]):'        # Match on hours
+                      '([0-5]?[0-9]):'              # Match on minutes
+                      '([0-5]?[0-9]).'              # Match on seconds
+                      '([0-9])+', long_timestamp)   # Match on nanoseconds
+
+    if match:
+        simple_time = str(match.group(0))
+        simple_time = convert_time_ns_ms(simple_time)
+    else:
+        simple_time = None
+
+    return simple_time
+
+
+def get_timestamp_from_pkt(pkt):
+    """
+    A function to get a timestamp from a packet
+    :param pkt: A pyshark.packet.packet.Packet object.
+    :return:    A string containing the timestamp the pkt arrived.
+    """
+    long_time = pkt.frame_info.time
+    simple_time = simplify_long_timestamp(long_time)
+
+    return simple_time
+
+
+def count_num_miss_pings_per_ip(pcap_file):
+    """
+    A function to count the number of ICMP pings that didn't get a response
+    per source IP address
+    :param pcap_file: A .pcap file e.g. C:/my_pcap_files/'my_capture.pcap'.
+    :return:
+    """
+    list_miss_pings = get_list_icmp_requests_no_response(pcap_file)
+    dict_miss_pings = defaultdict(int)
+
+    for pkt in list_miss_pings:
+        src_ip_addr = pkt.ip.src
+        dict_miss_pings[src_ip_addr] += 1
+
+    return dict_miss_pings
+
+
+def plot_scatter_graph_from_dict(graph_dict, xlabel, ylabel, title, save_name):
+    """
+    A function to plot a line graph, given a dictionary with suitable
+    key/value pairs.
+    :param graph_dict:   The dictionary with data to plot.
+    :param xlabel:       The label for the x-axis.
+    :param ylabel:       The label for the y-axis.
+    :param title:        The title of the graph.
+    :param save_name:    The name of the file we save to store the picture.
+    :return:             Nothing. But we will save the figure in a local
+                         directory.
+    """
+    plt.interactive(False)
+    key_list = graph_dict.keys()
+    value_list = graph_dict.values()
+    plt.scatter(key_list, value_list)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(fontsize=7, rotation=90)
+    plt.subplots_adjust(bottom=0.5)
+    plt.title(title)
+    plt.savefig(save_name)
+
+    return
+
+
+def plot_missing_pings(pcap_file, plot_title):
+    """
+    A function that plots the timestamp of missing pings.
+    :param pcap_file:  A .pcap file e.g. C:/my_pcap_files/'my_capture.pcap'.
+    :param plot_title: The name of the plot
+    :return:
+    """
+    list_miss_pings = get_list_icmp_requests_no_response(pcap_file)
+    dict_miss_pings = defaultdict(int)
+
+    for pkt in list_miss_pings:
+        time = get_timestamp_from_pkt(pkt)
+        dict_miss_pings[time] = 1
+
+    plot_scatter_graph_from_dict(dict_miss_pings,
+                                 'Timestamp',
+                                 'Missing Ping',
+                                 plot_title,
+                                 '%s.png' % plot_title)
+
+    return
